6f8b4e0f3e2fb380d0196e0f173bbea420cc7da8
Bob Grim <grimb300@hotmail.com>
2020-07-22 14:27:47 -0700

    add ejs

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8888;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'ejs');
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () =>
   console.log(`App listening at httpL//localhost:${port}`)
