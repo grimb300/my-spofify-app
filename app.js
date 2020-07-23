da47753fc20ca3a73591f9bfb438cd15aee6da67
Bob Grim <grimb300@hotmail.com>
2020-07-23 10:57:19 -0700

    add express-session

--- app.js
+++ app.js
@@ -11,13 +11,27 @@ require('dotenv').config();
 const request = require('request'); // "Request" library
 const cors = require('cors');
 const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 
 const stateKey = 'spotify_auth_state';
 
 // Middleware from Spotify example code (use cors and cookieParser, don't use static)
 // app.use(express.static(__dirname + '/public')).use(cors()).use(cookieParser());
-app.use(cors()).use(cookieParser());
+//
+// According to the express-session docs, cookie-parser is no longer needed and
+// may even result in issues if the secret isn't the same between the two middlewares.
+// app.use(cors()).use(cookieParser());
+app.use(cors());
+
+// Middleware to keep track of each user session, handles cookies for me as well
+const session = require('express-session');
+app.use(
+  session({
+    secret: 'I have a twelve inch cock. His name is Stewey!',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -47,12 +61,19 @@ const generateRandomString = function (length) {
 
 // Root route
 // app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  console.log(`Session (/):`);
+  console.log(req.session);
+  res.render('index');
+});
 
 // Login route, borrowed from the Spotify example code
 app.get('/login', (req, res) => {
+  console.log(`Session (/login):`);
+  console.log(req.session);
   const state = generateRandomString(16);
-  res.cookie(stateKey, state);
+  // res.cookie(stateKey, state);
+  req.session[stateKey] = state;
 
   // your application requests authorization
   const scope = 'user-read-private user-read-email';
@@ -70,19 +91,23 @@ app.get('/login', (req, res) => {
 
 // Callback route, borrowed from the Spotify example code
 app.get('/callback', (req, res) => {
+  console.log(`Session (/callback):`);
+  console.log(req.session);
   // your application requests refresh and access tokens
   // after checking the state parameter
 
   const code = req.query.code || null;
   const state = req.query.state || null;
-  const storedState = req.cookies ? req.cookies[stateKey] : null;
+  // const storedState = req.cookies ? req.cookies[stateKey] : null;
+  const storedState = req.session[stateKey] ? req.session[stateKey] : null;
 
   console.log(`/callback: state - ${state}, storedState - ${storedState}`);
 
   if (state === null || state !== storedState) {
     res.redirect('/#' + querystring.stringify({ error: 'state_mismatch' }));
   } else {
-    res.clearCookie(stateKey);
+    // res.clearCookie(stateKey);
+    req.session[stateKey] = null;
     // The Buffer object creation below throws a warning:
     // (node:2180) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the
     // Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.
@@ -109,25 +134,32 @@ app.get('/callback', (req, res) => {
         const access_token = body.access_token,
           refresh_token = body.refresh_token;
 
-        const options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { Authorization: 'Bearer ' + access_token },
-          json: true
-        };
+        // Add the tokens to the session data
+        req.session.access_token = access_token;
+        req.session.refresh_token = refresh_token;
 
-        // use the access token to access the Spotify Web API
-        request.get(options, (error, response, body) => {
-          // console.log(body);
-        });
+        // Redirect to the '/user' route
+        res.redirect('/user');
 
-        // we can also pass the token to the browser to make requests from there
-        res.redirect(
-          '/#' +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token
-            })
-        );
+        // const options = {
+        //   url: 'https://api.spotify.com/v1/me',
+        //   headers: { Authorization: 'Bearer ' + access_token },
+        //   json: true
+        // };
+
+        // // use the access token to access the Spotify Web API
+        // request.get(options, (error, response, body) => {
+        //   // console.log(body);
+        // });
+
+        // // we can also pass the token to the browser to make requests from there
+        // res.redirect(
+        //   '/#' +
+        //     querystring.stringify({
+        //       access_token: access_token,
+        //       refresh_token: refresh_token
+        //     })
+        // );
       } else {
         res.redirect('/#' + querystring.stringify({ error: 'invalid_token' }));
       }
@@ -135,8 +167,31 @@ app.get('/callback', (req, res) => {
   }
 });
 
+// User route
+app.get('/user', (req, res) => {
+  // Check to see if we have a token
+  if (req.session.access_token) {
+    // Get the user data
+    const options = {
+      url: 'https://api.spotify.com/v1/me',
+      headers: { Authorization: 'Bearer ' + req.session.access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log('User data successfully returned');
+        console.log(body);
+      }
+    });
+  }
+});
+
 // Refresh Token route, from Spotify example code
-app.get('/refresh_token', function (req, res) {
+app.get('/refresh_token', (req, res) => {
+  console.log(`Session (/refresh_token):`);
+  console.log(req.session);
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
   var authOptions = {
